04f65d2c038a3d0bce4a65b2f2c49ceb3d531b72
Davi de Medeiros <davi.demedeiros@here.com>
2021-12-24 00:56:09 +0100

    clean up example app to demo reconnectInterval function

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,29 @@
-import { useMemo, useState } from 'react';
-import useWebSocket from 'react-use-websocket';
+import useWebSocket, { Options } from 'react-use-websocket';
 import './App.css';
 
 const WS_URL = 'ws://localhost:1201';
-const WS_OPTIONS = { share: true };
-
-type Props = {
-  shouldConnect: boolean;
-};
-
-function Child(props: Props) {
-  // console.log('child: rendered');
-  const options = useMemo(
-    () => ({
-      ...WS_OPTIONS,
-      onOpen: () => console.log('child: opened'),
-      onError: () => console.log('child: errored'),
-      onMessage: () => console.log('child: received msg'),
-      onClose: () => console.log('child: closed'),
-    }),
-    []
-  );
-
-  useWebSocket(WS_URL, options, props.shouldConnect);
-
-  return <span>child.</span>;
-}
-
-function AnotherChild(props: Props) {
-  // console.log('another child: rendered');
-  const options = useMemo(
-    () => ({
-      ...WS_OPTIONS,
-      onOpen: () => console.log('another child: opened'),
-      onError: () => console.log('another child: errored'),
-      onMessage: () => console.log('another child: received msg'),
-      onClose: () => console.log('another child: closed'),
-    }),
-    []
-  );
-
-  useWebSocket(WS_URL, options, props.shouldConnect);
-
-  return <span>from another child.</span>;
-}
 
 export default function App() {
-  // console.log('app: rendered');
-  const [shouldConnect, setShouldConnect] = useState(true);
-  const options = useMemo(
-    () => ({
-      ...WS_OPTIONS,
-      onOpen: () => console.log('app: opened'),
-      onError: () => console.log('app: errored'),
-      onMessage: () => console.log('app: received msg'),
-      onClose: () => console.log('app: closed'),
-    }),
-    []
-  );
+  const options: Options = {
+    share: false,
+    onOpen: () => console.log('app: opened'),
+    onError: () => console.log('app: errored'),
+    onClose: () => console.log('app: closed'),
+    retryOnError: true,
+    shouldReconnect: () => true,
+    reconnectAttempts: 5,
+    // reconnectInterval: 2000,
+    reconnectInterval: async () => 2000,
+  };
 
-  useWebSocket(WS_URL, options, shouldConnect);
+  const { readyState } = useWebSocket(WS_URL, options)
 
   return (
     <article>
       <section>
-        <h1>
-          Hello, from <Child shouldConnect={shouldConnect} />
-        </h1>
-        <h2>
-          Hi, from <AnotherChild shouldConnect={shouldConnect} />
-        </h2>
-        <p>The web socket should be: {shouldConnect ? 'ON' : 'OFF'}</p>
+        <h1>Hello, world.</h1>
+        <p>The web socket should be: {readyState}</p>
       </section>
-      <button onClick={() => setShouldConnect(!shouldConnect)}>{shouldConnect ? 'Disconnect' : 'Connect'}</button>
     </article>
   );
 }
