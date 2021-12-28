831015bfbb958b957c3f8e40a526a0a71c6f7150
Davi de Medeiros <davi.demedeiros@here.com>
2021-12-28 18:24:31 +0100

    IMPROVEMENT: hds

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { HDSIcon } from '@here/hds-react-components'
 import useWebSocket, { Options } from 'react-use-websocket';
-import './App.css';
+import './App.scss';
 
 const WS_URL = 'ws://localhost:1201';
+
+let initialInterval: number;
+
+async function reconnectInterval(): Promise<number> {
+  initialInterval = initialInterval === undefined ? 2 : initialInterval * 2;
+  console.log(`Reconnecting in ${initialInterval}s`);
+
+  return initialInterval * 1000;
+}
 
 export default function App() {
   const options: Options = {
@@ -9,11 +19,11 @@ export default function App() {
     onOpen: () => console.log('app: opened'),
     onError: () => console.log('app: errored'),
     onClose: () => console.log('app: closed'),
-    retryOnError: true,
+    // retryOnError: true,
     shouldReconnect: () => true,
     reconnectAttempts: 5,
-    // reconnectInterval: 2000,
-    reconnectInterval: async () => 2000,
+    reconnectInterval,
+    // reconnectInterval: async () => 2000,
   };
 
   const { readyState } = useWebSocket(WS_URL, options)
@@ -21,6 +31,10 @@ export default function App() {
   return (
     <article>
       <section>
+        <HDSIcon name="here-logo" size="64" />
+        <HDSIcon name="plus" size="32" />
+        <HDSIcon name="substract" size="128" />
+        <HDSIcon name="driving" iconStyle="outline" size="64" />
         <h1>Hello, world.</h1>
         <p>The web socket should be: {readyState}</p>
       </section>
